69e7a2b7f4a51d12fd2b0e4eb48513c511a8323e
root <root@DESKTOP-6BEN2IT.localdomain>
2019-01-28 18:15:39 +0800

    change sh

--- build.sh
+++ build.sh
@@ -8,7 +8,5 @@ g++ -g -c ./lib/*.cpp -std=c++17
 mv *.o libso
 rm ./bin/* -rf
 g++ -g -o bin/chatServer example/chatServer.cpp ./protoFiles/*.o ./libso/*.o  -lpthread  -lprotobuf -std=c++17 
-#g++ -g -c example/chatServer.cpp  ./protoFiles/*.o ./libso/*.o  -lpthread  -lprotobuf -std=c++17
-g++ ./example/client.cpp -o ./bin/epoll_client
-g++ ./example/client2.cpp ./protoFiles/*.o  -lprotobuf -o ./bin/epoll_client2
+g++ ./example/client.cpp ./protoFiles/*.o  -lprotobuf -o ./bin/epoll_client
 ./bin/chatServer
