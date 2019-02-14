ff11ecefdaa9f6946d035d93e3d799a5b73de3f3
jaycn123 <1436734228@qq.com>
2019-02-14 11:41:01 +0800

    xxx

--- buildex.sh
+++ buildex.sh
@@ -8,6 +8,6 @@ g++ -g -c ./lib/*.cpp -std=c++17
 mv *.o libso
 rm ./bin/* -rf
 g++ -g -o bin/chatServer example/chatServer.cpp ./protoFiles/*.o ./libso/*.o  -lpthread  -lprotobuf -std=c++17 
-g++ ./example/client.cpp ./protoFiles/*.o  -lprotobuf -lpthread -o ./bin/epoll_client
+g++ ./example/client.cpp ./protoFiles/*.o  -lprotobuf -lpthread -o ./bin/epoll_client -std=c++17
 rm nohup.out
 nohup ./bin/chatServer
