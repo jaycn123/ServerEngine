d3f0ceb5b2c268fcf0049dbee6fec0e004f69d9b
jaycn123 <1436734228@qq.com>
2019-09-18 16:53:33 +0800

    xxx

---
+++ gateServer/gateServer.cpp
@@ -0,0 +1,79 @@
+#include "../lib/ConnectionManager.h"
+#include "../lib/ThreadPool.h"
+#include "../protoFiles/test.pb.h"
+#include "../lib/serviceBase.h"
+#include "../lib/type_define.h"
+#include "gateServer.h"
+#include "../lib/Log.h"
+
+CGameService::CGameService(void)
+{
+	if (!CLog::GetInstancePtr()->StartLog("gateServer", "log"))
+	{
+		std::cout << "error " << std::endl;
+	}
+	CLog::GetInstancePtr()->LogError("---------server start-----------");
+}
+
+CGameService::~CGameService(void)
+{
+
+}
+
+CGameService* CGameService::GetInstancePtr()
+{
+	static CGameService cGameService;
+	return &cGameService;
+}
+
+void CGameService::Init()
+{
+	ServiceBase::GetInstancePtr()->StartNetWork(8888,10000,this);
+}
+
+void CGameService::Uninit()
+{
+
+}
+
+void CGameService::Run()
+{
+	ServiceBase::GetInstancePtr()->Run();
+}
+
+bool CGameService::DispatchPacket(CNetPacket* pNetPacket)
+{
+	switch (pNetPacket->messId)
+	{
+		PROCESS_MESSAGE_ITEMEX(1, OnMsgWatchHeartBeatReq)
+	}
+}
+
+bool CGameService::OnMsgWatchHeartBeatReq(CNetPacket* pNetPacket)
+{
+	HeartBeatReq Req;
+	Req.ParsePartialFromArray(pNetPacket->m_pData, pNetPacket->m_len);
+
+	//std::cout << "OnMsgWatchHeartBeatReq : " << Req.connid() << std::endl;
+
+	//std::cout << "MsgFrom : " << pNetPacket->m_connId << std::endl << std::endl;
+
+	static uint32_t count = 1;
+
+	testSendProtobuf(pNetPacket->m_connId, Req.connid());
+}
+
+void CGameService::testSendProtobuf(uint32 connid,uint32_t tempcount)
+{
+	HeartBeatReq Req;
+	Req.set_connid(tempcount);
+	ServiceBase::GetInstancePtr()->SendMsgProtoBuf(connid, 1, Req);
+}
+
+int main()
+{
+	CGameService::GetInstancePtr()->Init();
+	CGameService::GetInstancePtr()->Run();
+	std::cout << "exit-----------------------------------------" << std::endl;
+	return 0;
+}
