5bd5090d43a1a21862c3219f79686f0bcd88fd33
jaycn123 <1436734228@qq.com>
2019-09-24 16:18:40 +0800

    xxx

---
+++ account/accountServer.cpp
@@ -0,0 +1,114 @@
+#include "../lib/ConnectionManager.h"
+#include "../lib/ThreadPool.h"
+#include "../protoFiles/test.pb.h"
+#include "../lib/serviceBase.h"
+#include "../lib/type_define.h"
+#include "accountServer.h"
+#include "../lib/Log.h"
+
+CGameService::CGameService(void)
+{
+	
+}
+
+CGameService::~CGameService(void)
+{
+
+}
+
+CGameService* CGameService::GetInstancePtr()
+{
+	static CGameService cGameService;
+	return &cGameService;
+}
+
+void CGameService::Init()
+{
+	if (!CLog::GetInstancePtr()->StartLog("gateServer", "log"))
+	{
+		std::cout << "error can not create log" << std::endl;
+	}
+
+	CLog::GetInstancePtr()->LogError("---------server start-----------");
+
+	if (!CConfigFile::GetInstancePtr()->Load("servercfg.ini"))
+	{
+		CLog::GetInstancePtr()->LogError("配制文件加载失败!");
+		return;
+	}
+
+	CLog::GetInstancePtr()->SetLogLevel(CConfigFile::GetInstancePtr()->GetIntValue("gate_log_level"));
+
+	uint32_t port = CConfigFile::GetInstancePtr()->GetIntValue("gate_svr_port");
+	uint32_t maxcon = CConfigFile::GetInstancePtr()->GetIntValue("gate_svr_max_con");
+
+
+	std::cout << "port : " << port << " maxcon : "<< maxcon << std::endl;
+
+
+	ServiceBase::GetInstancePtr()->StartNetWork(port, maxcon, this);
+}
+
+void CGameService::Uninit()
+{
+
+}
+
+void CGameService::Run()
+{
+	ServiceBase::GetInstancePtr()->Run();
+}
+
+bool CGameService::DispatchPacket(CNetPacket* pNetPacket)
+{
+	OnForwardNetPack(pNetPacket);
+// 	switch (pNetPacket->messId)
+// 	{
+// 		PROCESS_MESSAGE_ITEMEX(1, OnMsgWatchHeartBeatReq)
+// 	}
+
+}
+
+bool CGameService::OnForwardNetPack(CNetPacket* pNetPacket)
+{
+	HeartBeatReq Req;
+	Req.ParsePartialFromArray(pNetPacket->m_pData, pNetPacket->m_len);
+
+	std::cout << "OnMsgWatchHeartBeatReq : " << Req.connid() << std::endl;
+
+	//std::cout << "MsgFrom : " << pNetPacket->m_connId << std::endl << std::endl;
+
+	static uint32_t count = 1;
+
+	testSendProtobuf(pNetPacket->m_connId, Req.connid());
+	return true;
+}
+
+bool CGameService::OnMsgWatchHeartBeatReq(CNetPacket* pNetPacket)
+{
+	HeartBeatReq Req;
+	Req.ParsePartialFromArray(pNetPacket->m_pData, pNetPacket->m_len);
+
+	std::cout << "OnMsgWatchHeartBeatReq : " << Req.connid() << std::endl;
+
+	//std::cout << "MsgFrom : " << pNetPacket->m_connId << std::endl << std::endl;
+
+	static uint32_t count = 1;
+
+	testSendProtobuf(pNetPacket->m_connId, Req.connid());
+}
+
+void CGameService::testSendProtobuf(uint32 connid,uint32_t tempcount)
+{
+	HeartBeatReq Req;
+	Req.set_connid(tempcount);
+	ServiceBase::GetInstancePtr()->SendMsgProtoBuf(connid, 1, Req);
+}
+
+int main()
+{
+	CGameService::GetInstancePtr()->Init();
+	CGameService::GetInstancePtr()->Run();
+	std::cout << "exit-----------------------------------------" << std::endl;
+	return 0;
+}
---
+++ account/accountServer.h
@@ -0,0 +1,40 @@
+#ifndef CHATSERVER_H
+#define CHATSERVER_H
+
+#include "../lib/messageQueue.h"
+#include "../lib/configFile.h"
+
+
+class CGameService : public IPacketDispatcher
+{
+private:
+	CGameService(void);
+	virtual ~CGameService(void);
+
+public:
+	static CGameService* GetInstancePtr();
+
+	void		Init();
+
+	void		Uninit();
+
+	void		Run();
+
+	bool		DispatchPacket(CNetPacket* pNetPacket);
+
+public:
+
+	
+public:
+
+	bool OnForwardNetPack(CNetPacket* pNetPacket);
+
+	//*********************消息处理定义开始******************************
+	bool OnMsgWatchHeartBeatReq(CNetPacket* pNetPacket);
+	//*********************消息处理定义结束******************************
+
+	void testSendProtobuf(uint32 connid, uint32_t tempcount);
+
+};
+
+#endif
